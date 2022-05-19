15133b7a2cb6dd66f0737007642314f01308e1de
Evans Sitibekiso <jevacrism@gmail.com>
2022-05-19 11:08:38 +0200

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Mission from './components/Mission';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/missions" element={<Mission />} />
+        <Route path="/" element={<Rockets />} />
+      </Routes>
+    </>
   );
 }
 
