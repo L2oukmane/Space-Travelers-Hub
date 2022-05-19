c679d3a24fbc0710c8e1e640bb210762aae95153
Evans Sitibekiso <jevacrism@gmail.com>
2022-05-20 00:50:34 +0200

    changes on src files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
+import { fetchData } from './redux/missions/missions';
 import './index.css';
 import Rockets from './components/Rockets';
 import Profile from './components/Profile';
 import Mission from './components/Mission';
 
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
   return (
-
     <Router>
       <Navbar />
       <Routes>
@@ -16,7 +21,6 @@ export default function App() {
         <Route path="/missions" element={<Mission />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-
     </Router>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from '../redux/missions/missions';
+import styles from './styles/Profile.module.css';
 
-export default function Profile() {
-  return (
-    <div>Profile</div>
+const Profile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+  // console.log(missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(fetchData());
+    }
+  });
+
+  const missionReserve = missions.filter(
+    (mission) => mission.isUserJoinedToMission === true && mission,
   );
-}
+  console.log(missionReserve);
+  return (
+    <>
+      <section className={styles.profileSection}>
+        <div className={styles.missionsCard}>
+          <h2 className={styles.missionsName}>My Missions</h2>
+          {missionReserve.map((showMissions) => (
+            <div key={showMissions.id}>
+              <h4 className={styles.name}>{showMissions.mission_name}</h4>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Profile;
